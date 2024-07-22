cc8d177040493eef017190465902e425c5fc048f
HanustaviraGA <hanustavira.acarya@binus.ac.id>
2024-07-22 21:55:11 +0700

    Refactor verification

---
+++ handler/verificationHandler.js
@@ -0,0 +1,16 @@
+const AppReponseDto = require("../dtos/app_reponse.dto");
+class VerificationHandler{
+    constructor(verificationService){
+        this.verificationService = verificationService;
+        this.save = this.save.bind(this);
+    }
+
+    async save(req, res) {
+        const body = req.body;
+        const user_id = body.user_id;
+        const sendVerification = this.verificationService.save(user_id);
+        res.send(AppReponseDto.buildSuccessWithDto(sendVerification.status, sendVerification.message));
+    }
+}
+
+module.exports = VerificationHandler
